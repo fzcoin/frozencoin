4108e5fd3c88a5ac54ad094349bf2b798dd6f6d4
trenton <trenton@frozencoin-org>
2013-12-30 23:54:05 +0100

    Added entry "blockreward" to getmininginfo

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -79,6 +79,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("hashespersec",  gethashespersec(params, false)));
     obj.push_back(Pair("pooledtx",      (uint64_t)mempool.size()));
     obj.push_back(Pair("testnet",       fTestNet));
+    obj.push_back(Pair("blockreward",   FormatMoney( _GetBlockValue(nBestHeight,0,0),false)));
     return obj;
 }
 
