4265cf9f2ed2af93dc2318f70d3615b305a9f32c
frozencoin <frozencoin@gogol.(none)>
2014-01-01 13:31:39 +0100

    Replaced remaining references to QUARKCOIN with FROZEN

--- src/qt/locale/bitcoin_da.ts
+++ src/qt/locale/bitcoin_da.ts
@@ -248,8 +248,8 @@ Produktet indeholder software som er udviklet af OpenSSL Project til brug i Open
     </message>
     <message>
         <location line="+1"/>
-        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR QUARKCOINS&lt;/b&gt;!</source>
-        <translation>Advarsel: Hvis du krypterer din tegnebog og mister din adgangskode, vil du &lt;b&gt;MISTE ALLE DINE QUARKCOINS&lt;/b&gt;!</translation>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR FROZENS&lt;/b&gt;!</source>
+        <translation>Advarsel: Hvis du krypterer din tegnebog og mister din adgangskode, vil du &lt;b&gt;MISTE ALLE DINE FROZENS&lt;/b&gt;!</translation>
     </message>
     <message>
         <location line="+0"/>
