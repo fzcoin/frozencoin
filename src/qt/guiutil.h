a03e97cedb06d9f47e0928623498a41be6324554
trenton <trenton@frozencoin-org>
2014-01-07 00:21:57 +0100

    Cosmetics

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -96,7 +96,7 @@ namespace GUIUtil
     bool GetStartOnSystemStartup();
     bool SetStartOnSystemStartup(bool fAutoStart);
 
-    /** Help message for Bitcoin-Qt, shown with --help. */
+    /** Help message for Frozen-Qt, shown with --help. */
     class HelpMessageBox : public QMessageBox
     {
         Q_OBJECT
